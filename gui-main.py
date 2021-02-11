aba4e8bdd2f74b9d5177bf9eac1b6778698b082b
unknown <lovingisluck@naver.com>
2021-02-11 21:52:10 +0900

    random recommendation

--- gui-main.py
+++ gui-main.py
@@ -23,6 +23,36 @@ item_n = st.slider(
     'Select the number of keywords',
     0, 100, (0, 30)
 )
+# function option
+
+sch_op0, sch_op1 , sch_op2 = st.beta_columns(3)
+# # You can use a column just like st.sidebar:
+# left_column.button('Press me!')
+
+with sch_op0 :
+    op0 = st.checkbox(
+        'recommendation 3',
+        value = False,
+    )
+with sch_op1 :
+    op1 = st.checkbox(
+        'Beta 0 ',
+        value = False,
+    )
+with sch_op2 :
+    op2 = st.checkbox(
+        'Beta 1',
+        value = False,
+    )
+    
+# # Or even better, call Streamlit functions inside a "with" block:
+# with right_column:
+#     chosen = st.radio(
+#         'Sorting hat',
+#         ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+#     st.write(f"You are in {chosen} house!")
+
+
 
 if sch_btn:
     ktrend = kt(sch_kw)
@@ -35,13 +65,21 @@ if sch_btn:
     
     coords = ktrend.get_df_coordis(start=5)
     st.line_chart(coords)
+
+    # op0 : recommendation 3
+    if op0 == True :
+        if len(kwds10) > 3 :
+            rds = rd.sample(kwd10,3)
+            rds = kwds10[rds]
+            rcmd3 = pd.merge(kwds10,rds)
+            st.write(rcmd3)
     
+
+        
 
 ## get sample 3
 # kwds_opts = st.button('')
 # if kwds_opts :
-#     rds = rd.sample(kwd10,3)
-#     st.write(rds)
 
 # layout
 # Add a selectbox to the sidebar:
